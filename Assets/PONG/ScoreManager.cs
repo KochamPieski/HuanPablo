ee229bf384556826db36323dca93e253826f4cc5
Natalia Danielak <ndanielak@edu.cdv.pl>
2023-06-30 11:13:20 +0200

    player Score

---
+++ Assets/PONG/ScoreManager.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public TextMeshProUGUI leftPlayerScoreText;
+    public TextMeshProUGUI rightPlayerScoreText;
+    
+    public int leftPlayerScore;
+    public int rightPlayerScore;
+
+    private void Start()
+    {
+        leftPlayerScore = 0;
+        rightPlayerScore = 0;
+        leftPlayerScoreText.text = leftPlayerScore.ToString();
+        rightPlayerScoreText.text = rightPlayerScore.ToString();
+    }
+
+    public void IncrementLeftPlayerScore()
+    {
+        leftPlayerScore++;
+        leftPlayerScoreText.text = leftPlayerScore.ToString();
+    }
+    
+    public void IncrementRightPlayerScore()
+    {
+        rightPlayerScore++;
+        rightPlayerScoreText.text = rightPlayerScore.ToString();
+    }
+}
